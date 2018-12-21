92658bd967f45f67b345fadd4cd500f8bb008aa8
Gangaraju Battem(Raju) <gangaraju.battem@gmail.com>
2018-12-21 17:05:53 +0530

    testcommit

--- javatest/src/com/cotiviti/clientProfile/corejava/JavaTestOne.java
+++ javatest/src/com/cotiviti/clientProfile/corejava/JavaTestOne.java
@@ -3,6 +3,6 @@ package com.cotiviti.clientProfile.corejava;
 public class JavaTestOne {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("test12");
+		System.out.println("test12d");
 	}
 }
